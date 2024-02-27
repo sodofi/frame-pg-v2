6e75f80891f68a1231082658d22a541b280bf2ac
sophiahdew <sophiahdew@gmail.com>
2024-02-27 14:40:46 -0700

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,49 @@
+import { getFrameMetadata } from '@coinbase/onchainkit/frame';
+import type { Metadata } from 'next';
+import { NEXT_PUBLIC_URL } from './config';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: 'Go back',
+    },
+    {
+      action: 'link',
+      label: 'Click to donate',
+      target: 'https://www.google.com',
+    },
+    {
+      label: 'Next project',
+      action: 'post_redirect',
+    },
+  ],
+  image: {
+    src: `${NEXT_PUBLIC_URL}/park-3.png`,
+    aspectRatio: '1:1',
+  },
+  input: {
+    text: 'Donate to your fave PGs',
+  },
+  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+});
+
+export const metadata: Metadata = {
+  title: 'zizzamia.xyz',
+  description: 'LFG',
+  openGraph: {
+    title: 'zizzamia.xyz',
+    description: 'LFG',
+    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Frame PG :)</h1>
+    </>
+  );
+}
